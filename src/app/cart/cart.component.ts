0b179327f2a9c219a1f9e63f0772c6a63e595ec3
SalvatoreBonaccorso <salvo.bonaccorso89@gmail.com>
2020-02-05 12:43:46 +0100

    added a form for payment

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,16 +11,34 @@ export class CartComponent implements OnInit {
 
   // Define the 'items' property to store the products in the cart
   items;
+  // Define the 'checkoutForm' property to store the form model
+  checkoutForm;
 
   constructor(
     // Inject the cart service
-    private cartService: CartService
+    private cartService: CartService,
+    // Inject the form builder
+    private formBuilder: FormBuilder,
   ) {
-    
+     this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  
    }
 
+       // method to process the form
+    onSubmit(customerData) {
+      console.warn("You order has been submitted",customerData);
+
+      this.items = this.cartService.clearCart();
+      this.checkoutForm.reset();
+    }
+
   ngOnInit() {
-    this.items = this.cartService.getItems();
+   
   }
 
 }
